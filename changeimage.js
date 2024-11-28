04fb493c43d61c130f76c00ba42773d6bdf527fa
Your Name <mydo.17102003@gmail.com>
2024-11-28 18:29:19 +0700

    Prokect FUll

---
+++ changeimage.js
@@ -0,0 +1,6 @@
+/* Changeimage */
+function changeImage (id) {
+    let imagePath = document.getElementById(id).getAttribute('src');
+    
+    document.getElementById('img-main').setAttribute('src', imagePath);
+}
---
+++ toggle.js
@@ -0,0 +1,298 @@
+/* toggle */
+
+var btnh1 = document.getElementById('btn1');
+
+function toggle1(){
+    if (btnh1.style.color =="red") {
+        btnh1.style.color = "grey"
+    }
+    else{
+        btnh1.style.color = "red"
+    }
+}
+
+var btnh2 = document.getElementById('btn2');
+
+function toggle2(){
+    if (btnh2.style.color =="red") {
+        btnh2.style.color = "grey"
+    }
+    else{
+        btnh2.style.color = "red"
+    }
+}
+
+var btnh3 = document.getElementById('btn3');
+
+function toggle3(){
+    if (btnh3.style.color =="red") {
+        btnh3.style.color = "grey"
+    }
+    else{
+        btnh3.style.color = "red"
+    }
+}
+
+var btnh4 = document.getElementById('btn4');
+
+function toggle4(){
+    if (btnh4.style.color =="red") {
+        btnh4.style.color = "grey"
+    }
+    else{
+        btnh4.style.color = "red"
+    }
+}
+
+var btnh5 = document.getElementById('btn5');
+
+function toggle5(){
+    if (btnh5.style.color =="red") {
+        btnh5.style.color = "grey"
+    }
+    else{
+        btnh5.style.color = "red"
+    }
+}
+
+var btnh6 = document.getElementById('btn6');
+
+function toggle6(){
+    if (btnh6.style.color =="red") {
+        btnh6.style.color = "grey"
+    }
+    else{
+        btnh6.style.color = "red"
+    }
+}
+
+var btnh7 = document.getElementById('btn7');
+
+function toggle7(){
+    if (btnh7.style.color =="red") {
+        btnh7.style.color = "grey"
+    }
+    else{
+        btnh7.style.color = "red"
+    }
+}
+
+var btnh8 = document.getElementById('btn8');
+
+function toggle8(){
+    if (btnh8.style.color =="red") {
+        btnh8.style.color = "grey"
+    }
+    else{
+        btnh8.style.color = "red"
+    }
+}
+
+var btnh9 = document.getElementById('btn9');
+
+function toggle9(){
+    if (btnh9.style.color =="red") {
+        btnh9.style.color = "grey"
+    }
+    else{
+        btnh9.style.color = "red"
+    }
+}
+
+var btnh10 = document.getElementById('btn10');
+
+function toggle10(){
+    if (btnh10.style.color =="red") {
+        btnh10.style.color = "grey"
+    }
+    else{
+        btnh10.style.color = "red"
+    }
+}
+
+var btnh11 = document.getElementById('btn11');
+
+function toggle11(){
+    if (btnh11.style.color =="red") {
+        btnh11.style.color = "grey"
+    }
+    else{
+        btnh11.style.color = "red"
+    }
+}
+
+var btnh12 = document.getElementById('btn12');
+
+function toggle12(){
+    if (btnh12.style.color =="red") {
+        btnh12.style.color = "grey"
+    }
+    else{
+        btnh12.style.color = "red"
+    }
+}
+
+var btnh13 = document.getElementById('btn13');
+
+function toggle13(){
+    if (btnh13.style.color =="red") {
+        btnh13.style.color = "grey"
+    }
+    else{
+        btnh13.style.color = "red"
+    }
+}
+
+var btnh14 = document.getElementById('btn14');
+
+function toggle14(){
+    if (btnh14.style.color =="red") {
+        btnh14.style.color = "grey"
+    }
+    else{
+        btnh14.style.color = "red"
+    }
+}
+
+var btnh15 = document.getElementById('btn15');
+
+function toggle15(){
+    if (btnh15.style.color =="red") {
+        btnh15.style.color = "grey"
+    }
+    else{
+        btnh15.style.color = "red"
+    }
+}
+
+var btnh16 = document.getElementById('btn16');
+
+function toggle16(){
+    if (btnh16.style.color =="red") {
+        btnh16.style.color = "grey"
+    }
+    else{
+        btnh16.style.color = "red"
+    }
+}
+
+var btnh17 = document.getElementById('btn17');
+
+function toggle17(){
+    if (btnh17.style.color =="red") {
+        btnh17.style.color = "grey"
+    }
+    else{
+        btnh17.style.color = "red"
+    }
+}
+
+var btnh18 = document.getElementById('btn18');
+
+function toggle18(){
+    if (btnh18.style.color =="red") {
+        btnh18.style.color = "grey"
+    }
+    else{
+        btnh18.style.color = "red"
+    }
+}
+
+var btnh19 = document.getElementById('btn19');
+
+function toggle19(){
+    if (btnh19.style.color =="red") {
+        btnh19.style.color = "grey"
+    }
+    else{
+        btnh19.style.color = "red"
+    }
+}
+
+var btnh20 = document.getElementById('btn20');
+
+function toggle20(){
+    if (btnh20.style.color =="red") {
+        btnh20.style.color = "grey"
+    }
+    else{
+        btnh20.style.color = "red"
+    }
+}
+
+var btnh21 = document.getElementById('btn21');
+
+function toggle21(){
+    if (btnh21.style.color =="red") {
+        btnh21.style.color = "grey"
+    }
+    else{
+        btnh21.style.color = "red"
+    }
+}
+
+var btnh22 = document.getElementById('btn22');
+
+function toggle22(){
+    if (btnh22.style.color =="red") {
+        btnh22.style.color = "grey"
+    }
+    else{
+        btnh22.style.color = "red"
+    }
+}
+
+var btnh23 = document.getElementById('btn23');
+
+function toggle23(){
+    if (btnh23.style.color =="red") {
+        btnh23.style.color = "grey"
+    }
+    else{
+        btnh23.style.color = "red"
+    }
+}
+
+var btnh24 = document.getElementById('btn24');
+
+function toggle24(){
+    if (btnh24.style.color =="red") {
+        btnh24.style.color = "grey"
+    }
+    else{
+        btnh24.style.color = "red"
+    }
+}
+
+var btnh25 = document.getElementById('btn25');
+
+function toggle25(){
+    if (btnh25.style.color =="red") {
+        btnh25.style.color = "grey"
+    }
+    else{
+        btnh25.style.color = "red"
+    }
+}
+
+var btnh26 = document.getElementById('btn26');
+
+function toggle26(){
+    if (btnh26.style.color =="red") {
+        btnh26.style.color = "grey"
+    }
+    else{
+        btnh26.style.color = "red"
+    }
+}
+
+var btnh27 = document.getElementById('btn27');
+
+function toggle27(){
+    if (btnh27.style.color =="red") {
+        btnh27.style.color = "grey"
+    }
+    else{
+        btnh27.style.color = "red"
+    }
+}
